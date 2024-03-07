0be06b45fa7811e322df9006737b353b8c55ee5b
sbn raju <simhardrinagaraju@gmail.com>
2024-03-07 20:26:12 +0530

    New files

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ const signupRoutePost = require("./routes/signup.routes.js");
 //MIDDLEWARES
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname,"../public/css")));
 
 
 //DATABASE CONNECTIONS

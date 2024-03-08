8e94b42b4fe89ba105c498211b162cda02f6015e
sbn raju <simhardrinagaraju@gmail.com>
2024-03-08 14:13:50 +0530

    Added newfiles

--- src/index.js
+++ src/index.js
@@ -17,6 +17,8 @@ const signupRoutePost = require("./routes/signup.routes.js");
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"../public/css")));
+app.use(express.static(path.join(__dirname,"../public/js")));
+app.use(express.static(path.join(__dirname,"../public/images")));
 
 
 //DATABASE CONNECTIONS

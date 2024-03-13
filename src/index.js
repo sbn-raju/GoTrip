71fab98b89446f7c0d617c6a36217b8892e4e6f6
sbn raju <simhardrinagaraju@gmail.com>
2024-03-13 15:28:45 +0530

    New Pages Format

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const signupRoutePost = require("./routes/signup.routes.js");
 
 //MIDDLEWARES
 app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views/Pages"));
+app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"../public/css")));
 app.use(express.static(path.join(__dirname,"../public/js")));
 app.use(express.static(path.join(__dirname,"../public/images")));
